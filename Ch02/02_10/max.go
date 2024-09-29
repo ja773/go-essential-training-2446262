1d33565465e21ab2d7a566dbf6947119695a9af7
Jiyaad Ali <jiyaadali@outlook.com>
2024-09-29 05:45:00 +0100

    completed max challenge

--- Ch02/02_10/max.go
+++ Ch02/02_10/max.go
@@ -8,4 +8,15 @@ import (
 func main() {
 	nums := []int{16, 8, 42, 4, 23, 15}
 	fmt.Println(nums)
+
+	max := nums[0]
+
+	for _, val := range nums[1:] {
+		if val > max {
+			max = val
+		}
+	}
+
+	fmt.Println(max)
+
 }
